87b12adfe720ef3faf4402a3a599c08b2315b107
maiara <maiara.15muller@outlook.com>
2023-08-18 21:53:50 -0300

    aula 8

---
+++ index.js
@@ -0,0 +1,75 @@
+/*1. 
+console.log("a. ", false)
+console.log("b. ", false)
+console.log("c. ", true)
+console.log("d. ", typeof  Boolean)*/
+
+/*2 
+imprimiria no console o valor das duas variáveis,uma ao lado da outra*/
+
+/*3
+falta converter os valores das variáveis para number, se não irá ler como string.
+*/
+//Exercícios de escrita de código
+
+//1.a)
+
+/*const idade = prompt("Qual é a sua idade?");
+
+//b
+
+const idadeAmigo = prompt("Qual é a idade do seu amigo(a)?");
+
+const idadeMaior = idade > idadeAmigo;
+
+const diferenca = Number(idade) - Number(idadeAmigo);
+//c
+
+//console.log("Sua idade é maior do que a do seu melhor amigo?", idadeMaior);
+
+//d.
+
+console.log(diferenca);*/
+
+/*2.
+//a
+const numeroImpar = prompt("Escreva um número impar:");
+const restoDivisao = Number(numeroImpar) % 2;
+
+//b
+console.log(restoDivisao);
+
+//c. Notei que ao digitar números pares é exibido 0. 
+
+//d. E quando impar exibe 1.
+*/
+
+/*3.
+const anos = prompt("Quantos anos você tem?");
+const meses = 12;
+const dias = 365;
+const horas = Number(dias) * 24;
+
+const emMeses = Number(anos) * Number(meses);
+const emDias = Number(anos) * Number(dias);
+const emHoras = Number(anos) * Number(horas);
+
+console.log("A idade do usuário em meses", emMeses);
+console.log("A idade do usuário em dias", emDias);
+console.log("A idade do usuário em horas", emHoras);*/
+
+/*4.
+const numero1 = prompt("Digite um numero:");
+const numero2 = prompt("Digite outro numero:");
+
+const a = Number(numero1) > Number(numero2);
+const b = Number(numero1) == Number(numero2);
+const c = Number(numero1) % Number(numero2);
+const c2 = Number(numero2) % Number(numero1);
+const divisivel1 = c === 0;
+const divisivel2 = c2 === 0;
+
+console.log("O primeiro numero é maior que segundo?", a);
+console.log("O primeiro numero é igual ao segundo?", b);
+console.log("O primeiro numero é divisível pelo segundo?", divisivel1);
+console.log("O segundo numero é divisível pelo primeiro?", divisivel2);*/
